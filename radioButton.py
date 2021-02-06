1f9dbd4d325eb7c8ac7933ebe37c07a8ff855cb3
Avanish Gupta <ui19co15@iiitsurat.ac.in>
2021-02-06 17:32:54 +0530

    Added radio button with full functionality The radio buttons has been added but it displays the choice every time the buttons is clicked

--- radioButton.py
+++ radioButton.py
@@ -1,5 +1,4 @@
 from tkinter import *
-# from PIL import ImageTk, Image
 
 # root tkinter widget
 root = Tk()
@@ -8,6 +7,7 @@ root.title('Adding radio buttons')
 # r = IntVar()
 # r.set('2')
 
+# Branches to be selected by the user
 Branches = [
     ("CSE", "Computer Science and Engineering"),
     ("ECE", "Electronics and Communication Engineering"),
@@ -17,18 +17,20 @@ Branches = [
     ("EE", "Electrical Engineering"),
 ]
 
+# Declare a string type variable
 branch = StringVar()
+# Set a default value
 branch.set('Computer Science and Engineering')
 
+# Declare the radio buttons
 for short, full in Branches:
     Radiobutton(root, text=short, value=full, variable = branch).pack()
 
+# Define the function of the click button
 def clicked(value):
     myLabel = Label(root, text=value)
     myLabel.pack()
 
-# Radiobutton(root, text="YES", variable=r, value=1, command=lambda: clicked(r.get())).pack()
-# Radiobutton(root, text="NO", variable=r, value=2, command=lambda: clicked(r.get())).pack()
 
 # myLabel = Label(root, text=r.get())
 myLabel = Label(root, text=branch .get())
